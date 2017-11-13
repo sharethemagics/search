315a10b15b2fd28235c5e63f1629fe9f406c649f
Sathees Rajagopal <sharethemagics@gmail.com>
2017-11-13 17:38:12 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AngularMatModule } from './angularmat.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    BrowserAnimationsModule,
+    AngularMatModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
